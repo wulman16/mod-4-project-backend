e3d205757f4711c5c33421d0aa7cb74151f343c1
wulman16 <will.ulman@gmail.com>
2019-04-15 09:23:26 -0400

    revise controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show]
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def show
     render json: @user, status: :ok
   end
