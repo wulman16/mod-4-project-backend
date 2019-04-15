448acb58719fd5b52b6daee7726054b6f145de26
wulman16 <will.ulman@gmail.com>
2019-04-15 15:17:39 -0400

    allow patch requests

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module ClothingGame
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options]
       end
     end
 
